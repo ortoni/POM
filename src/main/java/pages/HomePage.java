3dba6fdfedb9f8ef39ceb8bde4d650fa727e301d
Koushik <koushik350@gmail.com>
2018-09-10 10:00:35 +0530

    Base Gradle Files

---
+++ src/main/java/pages/HomePage.java
@@ -0,0 +1,28 @@
+package pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import cucumber.api.java.en.And;
+import wdMethods.ProjectMethods;
+
+public class HomePage extends ProjectMethods {
+
+	public HomePage() {
+	PageFactory.initElements(driver, this);
+	}
+	@FindBy(className="decorativeSubmit")
+	WebElement eleLogOut;
+	@And("Click logout")
+	public LoginPage clickLogOut() {
+	click(eleLogOut);
+	return new LoginPage();
+	}
+}
+
+
+
+
+
+
+
---
+++ src/main/java/pages/LoginPage.java
@@ -0,0 +1,51 @@
+package pages;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+import wdMethods.ProjectMethods;
+
+public class LoginPage extends ProjectMethods {
+
+	public LoginPage() {
+		PageFactory.initElements(driver, this);
+	}
+
+	@FindBy(id="username")
+	WebElement eleUname;
+	@Given("Enter username as (.*)")
+	public LoginPage typeUsername(String data) {
+		type(eleUname, data);
+		return this;    
+	}
+	@FindBy(id="password")
+	WebElement elePassword;
+	@Given("Enter password as (.*)")
+	public LoginPage typePassword(String data) {
+		type(elePassword, data);
+		return this;
+	}
+	@FindBy(className="decorativeSubmit")
+	WebElement eleLogin;
+	@When("Click login button")
+	public HomePage clickLogin() {
+		click(eleLogin);
+		return new HomePage();		
+	}
+	@Then("verify that login is succesful")
+	public LoginPage verifyLogin() {
+	System.out.println("Verification logic");
+		return this;
+
+	}
+}
+
+
+
+
+
+
+
